d704a9ba92d8fce2272a456d1924b16e117cdfc1
shrikant_tathe <sptathe@gmail.com>
2024-01-12 19:52:29 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
+import React from 'react';
 import '../styles/App.css';
+import ReactDOM from 'react-dom';
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
+    <div>
+      <h1>React Learning</h1>
+      <p>I am learning React. My life is getting better.</p>
+    </div>
+  );
+};
 
 
 export default App;
