57fc59a4b5297b0dc17992297ef1c88f226e5c31
shrikant_tathe <sptathe@gmail.com>
2024-01-13 11:34:24 +0530

    solution5

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+const App =()=>{
   return (
     <div>
-       <h1><p>I am learning React. My life is getting better.</p></h1>
+       <p>I am learning React. My life is getting better.</p>
     </div>
   );
 };
